5412cf5fbe877577a4733bc044f31a7837f2ef00
Shivam Shukla <shivam@gkmit.co>
2022-12-14 15:40:43 +0530

    create navbar

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
 );
